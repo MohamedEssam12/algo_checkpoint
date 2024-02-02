615636c1dfa435a23c79619760187c986903c51a
Mohamed Essam <m.esam38208@gmail.com>
2024-02-02 21:17:44 +0200

    1

---
+++ 2/algo_checkpiont2_p1.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main ()
+{
+    // PROBLEM 1
+    int ary1[4] , ary2[5];
+    int sum = 0;
+    unsigned int i = 0 , j = 0;
+    
+    for (i = 0; i < 4; i++)
+    {
+        cout <<"please enter array1 values " << i<<": ";
+        cin>>ary1[i];
+    }
+    for (j = 0; j < 5; j++)
+    {
+        cout <<"please enter array2 values " <<j<<": ";
+        cin>>ary2[j];
+    }
+    
+    
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if (ary1[i] == ary2[j])
+            {
+                break;
+            }
+            else if ((ary1[i] != ary2[j]) && (j==4))
+            {
+                sum = sum + ary1[i];
+            }
+            
+        }
+        
+    }
+    cout<<"the first sum equals "<<sum<<endl;
+    for (j = 0; j < 5; j++)
+    {
+        for (i = 0; i< 4; i++)
+        {
+            if (ary2[j] == ary1[i])
+            {
+                break;
+            }
+            else
+            {
+                if (i==3)
+                {
+                    sum = sum + ary2[j];
+                }
+            }
+            
+        }
+        
+    }
+    
+    cout<<"the sum equals "<<sum<<endl;
+}
